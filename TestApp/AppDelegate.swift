268a3f881f29a0864d67f0c2f5351f25cd8043fd
Steve Ling <steve_ling@yahoo.com>
2018-02-04 19:31:37 -0800

    Initial Commit

---
+++ TestApp/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TestApp
+//
+//  Created by Steve Ling on 2/4/18.
+//  Copyright © 2018 Steve Ling. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
